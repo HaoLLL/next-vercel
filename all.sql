bd2c00888dbf56051e9b65cd57bb1208ed4db2dd
HaoL <m13522389185@163.com>
2023-03-06 17:42:00 +0800

    typeorm

--- all.sql
+++ all.sql
@@ -2,4 +2,6 @@
 id 用户名 密码 时间
 微信登录： 微信的用户表 和自己的用户表关联 id; user_id(自己表的); uid;微信唯一id； access_token:微信第三方令牌；access_expire:过期时间
 users: id nickName avatar 
+user_auths: id; user_id; identity_type; identifier; credential
 */
+create 
